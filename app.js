2983c7b2e588c579ce7e0468d50c35cdaad7e7f2
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-01-30 03:14:28 +0600

    subtotal fixed & shows ouput

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-// // plus minus button handler
-// function handleTicketChange(isIncrease) {
-//     const economyInput = document.getElementById('economy-count');
-//     const economyNumber = parseInt(economyInput.value);
-//     let economyNewCount = economyNumber;
-//     if (isIncrease == true) {
-//         economyNewCount = economyNumber + 1;
-//     }
-//     if (isIncrease == false && economyNewCount > 0) {
-//         economyNewCount = economyNumber - 1;
-//     }
-//     economyInput.value = economyNewCount;
-//     economySubTotal = economyNewCount * 100;
-//     document.getElementById("sub-total").innerText = '$' + economySubTotal;
-// };
-
-function handleTicketChange(isIncrease) {
-    const economyInput = document.getElementById('economy-count');
-    const economyNumber = parseInt(economyInput.value);
-    let economyNewCount = economyNumber;
+function handleTicketChange(ticket, isIncrease) {
+    const userTicket = document.getElementById(ticket + '-count');
+    const ticketNumber = parseInt(userTicket.value);
+    let ticketNewCount = ticketNumber;
     if (isIncrease == true) {
-        economyNewCount = economyNumber + 1;
+        ticketNewCount = ticketNumber + 1;
     }
-    if (isIncrease == false && economyNewCount > 0) {
-        economyNewCount = economyNumber - 1;
+    if (isIncrease == false && ticketNewCount > 0) {
+        ticketNewCount = ticketNumber - 1;
     }
-    economyInput.value = economyNewCount;
-    economySubTotal = economyNewCount * 100;
-    document.getElementById("sub-total").innerText = '$' + economySubTotal;
+    userTicket.value = ticketNewCount;
+    calculateTicketPrice();
 };
+
+function calculateTicketPrice() {
+    const firstClassTicket = document.getElementById('firstClass-count');
+    const firstClassTicketNumber = parseInt(firstClassTicket.value);
+    const economyTicket = document.getElementById('economy-count');
+    const economyTicketNumber = parseInt(economyTicket.value);
+    const subTotal = firstClassTicketNumber * 150 + economyTicketNumber * 100;
+    document.getElementById("sub-total").innerText = '$' + subTotal;
+}
